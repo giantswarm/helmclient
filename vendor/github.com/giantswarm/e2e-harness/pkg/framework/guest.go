feca5bdc26df6938fa37767132b8e14a0312456e
Tim Schindler <xh3b4sd@gmail.com>
2018-06-11 12:46:39 +0200

    k8s 1.10.4 (#27)

--- vendor/github.com/giantswarm/e2e-harness/pkg/framework/guest.go
+++ vendor/github.com/giantswarm/e2e-harness/pkg/framework/guest.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/cenkalti/backoff"
+	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"k8s.io/api/core/v1"
@@ -29,6 +30,7 @@ type GuestConfig struct {
 type Guest struct {
 	logger micrologger.Logger
 
+	g8sClient  versioned.Interface
 	k8sClient  kubernetes.Interface
 	restConfig *rest.Config
 }
@@ -41,6 +43,7 @@ func NewGuest(config GuestConfig) (*Guest, error) {
 	g := &Guest{
 		logger: config.Logger,
 
+		g8sClient:  nil,
 		k8sClient:  nil,
 		restConfig: nil,
 	}
@@ -48,15 +51,22 @@ func NewGuest(config GuestConfig) (*Guest, error) {
 	return g, nil
 }
 
+// G8sClient returns the guest cluster framework's apiextensions clientset. The
+// client being returned is properly configured once Guest.Setup() is executed
+// successfully.
+func (g *Guest) G8sClient() versioned.Interface {
+	return g.g8sClient
+}
+
 // K8sClient returns the guest cluster framework's Kubernetes client. The client
-// being returned is properly configured ones Guest.Setup() got executed
+// being returned is properly configured once Guest.Setup() is executed
 // successfully.
 func (g *Guest) K8sClient() kubernetes.Interface {
 	return g.k8sClient
 }
 
 // RestConfig returns the guest cluster framework's rest config. The config
-// being returned is properly configured ones Guest.Setup() got executed
+// being returned is properly configured once Guest.Setup() is executed
 // successfully.
 func (g *Guest) RestConfig() *rest.Config {
 	return g.restConfig
@@ -76,6 +86,7 @@ func (g *Guest) Initialize() error {
 		}
 	}
 
+	var guestG8sClient versioned.Interface
 	var guestK8sClient kubernetes.Interface
 	var guestRestConfig *rest.Config
 	{
@@ -86,12 +97,17 @@ func (g *Guest) Initialize() error {
 		}
 
 		guestRestConfig = &rest.Config{
-			Host: os.ExpandEnv("https://api.${CLUSTER_NAME}.${COMMON_DOMAIN_GUEST}"),
+			Host: os.ExpandEnv("https://api.${CLUSTER_NAME}.k8s.${COMMON_DOMAIN}"),
 			TLSClientConfig: rest.TLSClientConfig{
 				CAData:   s.Data["ca"],
 				CertData: s.Data["crt"],
 				KeyData:  s.Data["key"],
 			},
+		}
+
+		guestG8sClient, err = versioned.NewForConfig(guestRestConfig)
+		if err != nil {
+			return microerror.Mask(err)
 		}
 
 		guestK8sClient, err = kubernetes.NewForConfig(guestRestConfig)
@@ -100,6 +116,7 @@ func (g *Guest) Initialize() error {
 		}
 	}
 
+	g.g8sClient = guestG8sClient
 	g.k8sClient = guestK8sClient
 	g.restConfig = guestRestConfig
 
