7100e7a7cefb9b7348b13b923a2d6143c38b1156
Paweł Kopiczko <kopiczko@gmail.com>
2018-09-19 11:58:04 +0100

    fix tiller not retry found handling

--- helmclient.go
+++ helmclient.go
@@ -94,6 +94,8 @@ func (c *Client) DeleteRelease(releaseName string, options ...helmclient.DeleteO
 		_, err = c.newHelmClientFromTunnel(t).DeleteRelease(releaseName, options...)
 		if IsReleaseNotFound(err) {
 			return backoff.Permanent(microerror.Maskf(releaseNotFoundError, releaseName))
+		} else if IsTillerNotFound(err) {
+			return backoff.Permanent(microerror.Mask(err))
 		} else if err != nil {
 			return microerror.Mask(err)
 		}
@@ -280,6 +282,8 @@ func (c *Client) GetReleaseContent(releaseName string) (*ReleaseContent, error) 
 			resp, err = c.newHelmClientFromTunnel(t).ReleaseContent(releaseName)
 			if IsReleaseNotFound(err) {
 				return backoff.Permanent(microerror.Maskf(releaseNotFoundError, releaseName))
+			} else if IsTillerNotFound(err) {
+				return backoff.Permanent(microerror.Mask(err))
 			} else if err != nil {
 				return microerror.Mask(err)
 			}
@@ -333,6 +337,8 @@ func (c *Client) GetReleaseHistory(releaseName string) (*ReleaseHistory, error) 
 			resp, err = c.newHelmClientFromTunnel(t).ReleaseHistory(releaseName, helmclient.WithMaxHistory(1))
 			if IsReleaseNotFound(err) {
 				return backoff.Permanent(microerror.Maskf(releaseNotFoundError, releaseName))
+			} else if IsTillerNotFound(err) {
+				return backoff.Permanent(microerror.Mask(err))
 			} else if err != nil {
 				return microerror.Mask(err)
 			}
@@ -382,10 +388,12 @@ func (c *Client) InstallFromTarball(path, ns string, options ...helmclient.Insta
 		defer c.closeTunnel(t)
 
 		release, err := c.newHelmClientFromTunnel(t).InstallRelease(path, ns, options...)
-		if IsReleaseNotFound(err) {
-			return backoff.Permanent(releaseNotFoundError)
-		} else if IsCannotReuseRelease(err) {
+		if IsCannotReuseRelease(err) {
 			return backoff.Permanent(cannotReuseReleaseError)
+		} else if IsReleaseNotFound(err) {
+			return backoff.Permanent(releaseNotFoundError)
+		} else if IsTillerNotFound(err) {
+			return backoff.Permanent(microerror.Mask(err))
 		} else if err != nil {
 			if IsInvalidGZipHeader(err) {
 				content, readErr := ioutil.ReadFile(path)
@@ -444,6 +452,8 @@ func (c *Client) RunReleaseTest(releaseName string, options ...helmclient.Releas
 	resChan, errChan := c.newHelmClientFromTunnel(t).RunReleaseTest(releaseName, helmclient.ReleaseTestTimeout(int64(runReleaseTestTimout)))
 	if IsReleaseNotFound(err) {
 		return backoff.Permanent(microerror.Maskf(releaseNotFoundError, releaseName))
+	} else if IsTillerNotFound(err) {
+		return backoff.Permanent(microerror.Mask(err))
 	} else if err != nil {
 		return microerror.Mask(err)
 	}
