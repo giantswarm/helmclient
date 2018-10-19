f994c18fc5d9d8f154c6188ed8466b71d8a1128e
Paweł Kopiczko <kopiczko@gmail.com>
2018-10-19 16:23:50 +0100

    integration: improve setup (#49)

--- integration/test/basic/main_test.go
+++ integration/test/basic/main_test.go
@@ -3,48 +3,29 @@
 package basic
 
 import (
+	"fmt"
 	"testing"
 
-	"github.com/giantswarm/e2e-harness/pkg/framework"
 	"github.com/giantswarm/helmclient/integration/setup"
-	"github.com/giantswarm/micrologger"
 )
 
 var (
-	h *framework.Host
+	config setup.Config
 )
 
 func init() {
 	var err error
 
-	var logger micrologger.Logger
 	{
-		c := micrologger.Config{}
-
-		logger, err = micrologger.New(c)
+		config, err = setup.NewConfig()
 		if err != nil {
-			panic(err.Error())
+			panic(fmt.Sprintf("%#v", err))
 		}
 	}
-
-	{
-		c := framework.HostConfig{
-			Logger: logger,
-
-			ClusterID:  "someval",
-			VaultToken: "someval",
-		}
-
-		h, err = framework.NewHost(c)
-		if err != nil {
-			panic(err.Error())
-		}
-	}
-
 }
 
 // TestMain allows us to have common setup and teardown steps that are run
 // once for all the tests https://golang.org/pkg/testing/#hdr-Main.
 func TestMain(m *testing.M) {
-	setup.WrapTestMain(h, m)
+	setup.Setup(m, config)
 }
