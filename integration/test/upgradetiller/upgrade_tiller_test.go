d5f18dbc68aee737bfa42aa1a9239bd2c1624a33
Ross Fairbanks <ross@giantswarm.io>
2018-12-12 14:32:03 +0100

    WIP: test with latest tiller image

--- integration/test/upgradetiller/upgrade_tiller_test.go
+++ integration/test/upgradetiller/upgrade_tiller_test.go
@@ -19,8 +19,8 @@ func TestUpgradeTiller(t *testing.T) {
 
 	var err error
 
-	currentTillerImage := "quay.io/giantswarm/tiller:v2.12.0"
-	outdatedTillerImage := "gcr.io/kubernetes-helm:v2.8.2"
+	currentTillerImage := "quay.io/giantswarm/tiller:v2.8.2"
+	outdatedTillerImage := "quay.io/giantswarm/tiller:v2.8.2"
 
 	labelSelector := "app=helm,name=tiller"
 	tillerNamespace := "giantswarm"
