099f8742093e266d35bba89f7afb05f5d54e71b4
Ross Fairbanks <ross@giantswarm.io>
2019-01-08 17:34:17 +0100

    Install chartmuseum for testing PullChartTarball

--- integration/test/pullcharttarball/pull_chart_tarball_test.go
+++ integration/test/pullcharttarball/pull_chart_tarball_test.go
@@ -7,15 +7,37 @@ import (
 	"os"
 	"testing"
 
+	"k8s.io/helm/pkg/helm"
+
 	"github.com/giantswarm/helmclient/integration/charttarball"
 	"github.com/giantswarm/helmclient/key"
 )
 
+// TestPullChartTarball starts a chartmuseum server and pushes a test chart.
+// PullChartTarball is used to download the chart and LoadChart is used to
+// parse it. Finally the metadata for the chart is checked.
 func TestPullChartTarball(t *testing.T) {
 	ctx := context.Background()
 	var err error
 
-	const releaseName = "test"
+	chartMuseumRelease := "chartmuseum"
+	chartMuseumTarball, err := charttarball.Create("chartmuseum-chart")
+	if err != nil {
+		t.Fatalf("could not create chartmuseum archive %#v", err)
+	}
+	defer os.Remove(chartMuseumTarball)
+
+	err = config.HelmClient.EnsureTillerInstalled(ctx)
+	if err != nil {
+		t.Fatalf("could not install Tiller %#v", err)
+	}
+
+	// We need to pass the ValueOverrides option to make the install process
+	// use the default values and prevent errors on nested values.
+	err = config.HelmClient.InstallReleaseFromTarball(ctx, chartMuseumTarball, "default", helm.ReleaseName(chartMuseumRelease), helm.ValueOverrides([]byte("{}")))
+	if err != nil {
+		t.Fatalf("failed to install release %#q %#v", chartMuseumRelease, err)
+	}
 
 	tarballPath, err := charttarball.Create("test-chart")
 	if err != nil {
