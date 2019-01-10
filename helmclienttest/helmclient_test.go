3a8914bce1ed373e2215c57373d303fefadff2a4
Ross Fairbanks <ross@giantswarm.io>
2019-01-10 15:06:49 +0100

    Don't return error in helmclienttest constructor

--- helmclienttest/helmclient_test.go
+++ helmclienttest/helmclient_test.go
@@ -5,11 +5,6 @@ import (
 )
 
 func Test_New(t *testing.T) {
-	c := Config{}
-
 	// Test that New doesn't panic and helmclient.Interface is implemented.
-	_, err := New(c)
-	if err != nil {
-		t.Fatalf("error == %#v, want nil", err)
-	}
+	New(Config{})
 }
