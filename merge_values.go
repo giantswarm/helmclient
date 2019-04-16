c86fa0d92e8ad64713e363c138b5e4d70284a5db
Ross Fairbanks <ross@giantswarm.io>
2019-04-16 10:13:34 +0200

    Add YAML to string map

---
+++ merge_values.go
@@ -0,0 +1,57 @@
+package helmclient
+
+import (
+	"fmt"
+
+	"github.com/giantswarm/microerror"
+	yaml "gopkg.in/yaml.v2"
+)
+
+// yamlToStringMap unmarshals the YAML input into a map[string]interface{}
+// with string keys. This is necessary because the default behaviour of the
+// YAML parser is to return map[interface{}]interface{} types.
+// See https://github.com/go-yaml/yaml/issues/139.
+//
+func yamlToStringMap(input []byte) (map[string]interface{}, error) {
+	var raw interface{}
+	var result map[string]interface{}
+
+	err := yaml.Unmarshal(input, &raw)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	output := processMapValue(raw)
+	result = output.(map[string]interface{})
+
+	return result, nil
+}
+
+func processInterfaceArray(in []interface{}) []interface{} {
+	res := make([]interface{}, len(in))
+	for i, v := range in {
+		res[i] = processMapValue(v)
+	}
+	return res
+}
+
+func processInterfaceMap(in map[interface{}]interface{}) map[string]interface{} {
+	res := make(map[string]interface{})
+	for k, v := range in {
+		res[fmt.Sprintf("%v", k)] = processMapValue(v)
+	}
+	return res
+}
+
+func processMapValue(v interface{}) interface{} {
+	switch v := v.(type) {
+	case []interface{}:
+		return processInterfaceArray(v)
+	case map[interface{}]interface{}:
+		return processInterfaceMap(v)
+	case string:
+		return v
+	default:
+		return fmt.Sprintf("%v", v)
+	}
+}
---
+++ merge_values_test.go
@@ -0,0 +1,116 @@
+package helmclient
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+const (
+	deepNestedYaml = `
+nested:
+  deeper:
+    value: "deeper"
+test: test`
+	nestedArrayYaml = `
+nested:
+  array:
+  - 1: "test 1"
+  - 2: "test 2"
+test: test`
+	nestedArrayAndMapYaml = `
+nested:
+  another: "test"
+  array:
+  - 1: "test 1"
+  - 2: "test 2"
+test: test`
+	simpleNestedYaml = `
+nested:
+  value: "nested"
+test: test`
+)
+
+func Test_yamlToStringMap(t *testing.T) {
+	testCases := []struct {
+		name           string
+		input          []byte
+		expectedValues map[string]interface{}
+		errorMatcher   func(error) bool
+	}{
+		{
+			name:  "case 0: simple yaml",
+			input: []byte("test: test"),
+			expectedValues: map[string]interface{}{
+				"test": "test",
+			},
+		},
+		{
+			name:  "case 1: simple nested maps",
+			input: []byte(simpleNestedYaml),
+			expectedValues: map[string]interface{}{
+				"nested": map[string]interface{}{
+					"value": "nested",
+				},
+				"test": "test",
+			},
+		},
+		{
+			name:  "case 2: nested array",
+			input: []byte(nestedArrayYaml),
+			expectedValues: map[string]interface{}{
+				"nested": map[string]interface{}{
+					"array": []interface{}{
+						map[string]interface{}{
+							"1": "test 1",
+						},
+						map[string]interface{}{
+							"2": "test 2",
+						},
+					},
+				},
+				"test": "test",
+			},
+		},
+		{
+			name:  "case 3: nested array and map",
+			input: []byte(nestedArrayAndMapYaml),
+			expectedValues: map[string]interface{}{
+				"nested": map[string]interface{}{
+					"another": "test",
+					"array": []interface{}{
+						map[string]interface{}{
+							"1": "test 1",
+						},
+						map[string]interface{}{
+							"2": "test 2",
+						},
+					},
+				},
+				"test": "test",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := yamlToStringMap(tc.input)
+
+			switch {
+			case err == nil && tc.errorMatcher == nil:
+				// correct; carry on
+			case err != nil && tc.errorMatcher == nil:
+				t.Fatalf("error == %#v, want nil", err)
+			case err == nil && tc.errorMatcher != nil:
+				t.Fatalf("error == nil, want non-nil")
+			case !tc.errorMatcher(err):
+				t.Fatalf("error == %#v, want matching", err)
+			}
+
+			if !reflect.DeepEqual(result, tc.expectedValues) {
+				t.Fatalf("want matching values \n %s", cmp.Diff(result, tc.expectedValues))
+			}
+		})
+	}
+}
