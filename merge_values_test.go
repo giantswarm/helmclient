5e1daa4d201a9cce95023a2591874088b58e44fb
Ross Fairbanks <rossf7@users.noreply.github.com>
2019-04-24 16:50:33 +0200

    Add support for merging values (#94)
    * dep: add go-cmp
    
    * Add MergeValues
    
    * Fix typo
    
    * Refactor and move to separate files
    
    * Updated logic and improved godoc
    
    * Link to upstream mergeValues in godoc
    
    * Updated type handling for merge values
    
    * Change error type
    
    * Fix broken merge
    
    * Fix merge
    
    * Only support a single key for YAML in input maps
    
    * Updated godoc and error message
    
    * Address review comments

--- merge_values_test.go
+++ merge_values_test.go
@@ -7,32 +7,35 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-const (
-	deepNestedYaml = `
+const deepNestedYaml = `
 nested:
   deeper:
     value: "deeper"
 test: test`
-	mixedTypesYaml = `
+
+const mixedTypesYaml = `
 bool: true
 int: 1047552
 float: 274877.906944
 string: test
 text: test with a sentence`
-	nestedArrayYaml = `
+
+const nestedArrayYaml = `
 nested:
   array:
   - 1: "test 1"
   - 2: "test 2"
 test: test`
-	nestedArrayAndMapYaml = `
+
+const nestedArrayAndMapYaml = `
 nested:
   another: "test"
   array:
   - 1: "test 1"
   - 2: "test 2"
 test: test`
-	nestedMixedTypesYaml = `
+
+const nestedMixedTypesYaml = `
 nested:
   another: "test"
   array:
@@ -42,11 +45,133 @@ nested:
     bottom: true
     float: 274877.906944
 test: test`
-	simpleNestedYaml = `
+
+const overrideNestedYaml = `
+nested:
+  value: "replaced"
+test: test`
+
+const simpleNestedYaml = `
 nested:
   value: "nested"
 test: test`
-)
+
+func Test_MergeValues(t *testing.T) {
+	testCases := []struct {
+		name           string
+		destMap        map[string][]byte
+		srcMap         map[string][]byte
+		expectedValues map[string]interface{}
+		errorMatcher   func(error) bool
+	}{
+		{
+			name:           "case 0: empty dest and src, expected empty",
+			destMap:        map[string][]byte{},
+			srcMap:         map[string][]byte{},
+			expectedValues: map[string]interface{}{},
+		},
+		{
+			name:    "case 1: empty dest, non-empty src, expected src",
+			destMap: map[string][]byte{},
+			srcMap: map[string][]byte{
+				"values": []byte("test: val"),
+			},
+			expectedValues: map[string]interface{}{
+				"test": "val",
+			},
+		},
+		{
+			name: "case 2: non-empty dest with mixed types, empty src, expected dest",
+			destMap: map[string][]byte{
+				"values": []byte(mixedTypesYaml),
+			},
+			srcMap: map[string][]byte{},
+			expectedValues: map[string]interface{}{
+				"bool":   true,
+				"int":    1047552,
+				"float":  274877.906944,
+				"string": "test",
+				"text":   "test with a sentence",
+			},
+		},
+		{
+			name: "case 3: non-empty non-intersecting values",
+			destMap: map[string][]byte{
+				"deep": []byte(deepNestedYaml),
+			},
+			srcMap: map[string][]byte{
+				"simple": []byte(simpleNestedYaml),
+			},
+			expectedValues: map[string]interface{}{
+				"nested": map[string]interface{}{
+					"deeper": map[string]interface{}{
+						"value": "deeper",
+					},
+					"value": "nested",
+				},
+				"test": "test",
+			},
+		},
+		{
+			name: "case 4: non-empty intersecting values, src map is preferred",
+			destMap: map[string][]byte{
+				"simple": []byte(simpleNestedYaml),
+			},
+			srcMap: map[string][]byte{
+				"override": []byte(overrideNestedYaml),
+			},
+			expectedValues: map[string]interface{}{
+				"nested": map[string]interface{}{
+					"value": "replaced",
+				},
+				"test": "test",
+			},
+		},
+		{
+			name: "case 5: src map with multiple keys returns error",
+			destMap: map[string][]byte{
+				"test": []byte("test: val"),
+			},
+			srcMap: map[string][]byte{
+				"another": []byte("test: val"),
+				"test":    []byte("test: val"),
+			},
+			errorMatcher: IsExecutionFailed,
+		},
+		{
+			name: "case 6: dest map with multiple keys returns error",
+			destMap: map[string][]byte{
+				"another": []byte("test: val"),
+				"test":    []byte("test: val"),
+			},
+			srcMap: map[string][]byte{
+				"test": []byte("test: val"),
+			},
+			errorMatcher: IsExecutionFailed,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := MergeValues(tc.destMap, tc.srcMap)
+
+			switch {
+			case err == nil && tc.errorMatcher == nil:
+				// correct; carry on
+			case err != nil && tc.errorMatcher == nil:
+				t.Fatalf("error == %#v, want nil", err)
+			case err == nil && tc.errorMatcher != nil:
+				t.Fatalf("error == nil, want non-nil")
+			case !tc.errorMatcher(err):
+				t.Fatalf("error == %#v, want matching", err)
+			}
+
+			if !reflect.DeepEqual(result, tc.expectedValues) {
+				t.Fatalf("want matching values \n %s", cmp.Diff(result, tc.expectedValues))
+			}
+		})
+	}
+}
 
 func Test_yamlToStringMap(t *testing.T) {
 	testCases := []struct {
