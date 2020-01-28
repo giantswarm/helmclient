81dc42c4f47d8802b5562624c5c73bb7f2f9228a
Ross Fairbanks <ross@rossfairbanks.com>
2020-01-28 13:21:22 +0100

    Remove helm2 imports and align interface with helm 3

--- helmclient.go
+++ helmclient.go
@@ -11,9 +11,6 @@ import (
 	"github.com/spf13/afero"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/helm/pkg/chartutil"
-	helmclient "k8s.io/helm/pkg/helm"
-	hapichart "k8s.io/helm/pkg/proto/hapi/chart"
 )
 
 // Config represents the configuration used to create a helm client.
@@ -23,7 +20,7 @@ type Config struct {
 	// client. If this is nil, a new helm client will be created. Setting the
 	// helm client here manually might only be sufficient for testing or
 	// whenever you know what you do.
-	HelmClient helmclient.Interface
+	HelmClient Interface
 	K8sClient  kubernetes.Interface
 	Logger     micrologger.Logger
 
@@ -33,7 +30,7 @@ type Config struct {
 // Client knows how to talk with a Helm Tiller server.
 type Client struct {
 	fs         afero.Fs
-	helmClient helmclient.Interface
+	helmClient Interface
 	httpClient *http.Client
 	k8sClient  kubernetes.Interface
 	logger     micrologger.Logger
@@ -76,13 +73,13 @@ func New(config Config) (*Client, error) {
 }
 
 // DeleteRelease uninstalls a chart given its release name.
-func (c *Client) DeleteRelease(ctx context.Context, releaseName string, options ...helmclient.DeleteOption) error {
+func (c *Client) DeleteRelease(ctx context.Context, releaseName string) error {
 	eventName := "delete_release"
 
 	t := prometheus.NewTimer(histogram.WithLabelValues(eventName))
 	defer t.ObserveDuration()
 
-	err := c.deleteRelease(ctx, releaseName, options...)
+	err := c.deleteRelease(ctx, releaseName)
 	if err != nil {
 		errorGauge.WithLabelValues(eventName).Inc()
 		return microerror.Mask(err)
@@ -91,7 +88,7 @@ func (c *Client) DeleteRelease(ctx context.Context, releaseName string, options 
 	return nil
 }
 
-func (c *Client) deleteRelease(ctx context.Context, releaseName string, options ...helmclient.DeleteOption) error {
+func (c *Client) deleteRelease(ctx context.Context, releaseName string) error {
 	c.logger.LogCtx(ctx, "level", "debug", "message", "delete release not yet implemented for helm 3")
 	return nil
 }
@@ -143,13 +140,13 @@ func (c *Client) getReleaseHistory(ctx context.Context, releaseName string) (*Re
 }
 
 // InstallReleaseFromTarball installs a chart packaged in the given tarball.
-func (c *Client) InstallReleaseFromTarball(ctx context.Context, path, ns string, options ...helmclient.InstallOption) error {
+func (c *Client) InstallReleaseFromTarball(ctx context.Context, path, ns string, options InstallOptions) error {
 	eventName := "install_release_from_tarball"
 
 	t := prometheus.NewTimer(histogram.WithLabelValues(eventName))
 	defer t.ObserveDuration()
 
-	err := c.installReleaseFromTarball(ctx, path, ns, options...)
+	err := c.installReleaseFromTarball(ctx, path, ns, options)
 	if err != nil {
 		errorGauge.WithLabelValues(eventName).Inc()
 		return microerror.Mask(err)
@@ -158,7 +155,7 @@ func (c *Client) InstallReleaseFromTarball(ctx context.Context, path, ns string,
 	return nil
 }
 
-func (c *Client) installReleaseFromTarball(ctx context.Context, path, ns string, options ...helmclient.InstallOption) error {
+func (c *Client) installReleaseFromTarball(ctx context.Context, path, ns string, options InstallOptions) error {
 	c.logger.LogCtx(ctx, "level", "debug", "message", "install release from tarball not yet implemented for helm 3")
 	return nil
 }
@@ -201,29 +198,20 @@ func (c *Client) LoadChart(ctx context.Context, chartPath string) (Chart, error)
 }
 
 func (c *Client) loadChart(ctx context.Context, chartPath string) (Chart, error) {
-	helmChart, err := chartutil.Load(chartPath)
-	if err != nil {
-		return Chart{}, microerror.Mask(err)
-	}
-
-	chart, err := newChart(helmChart)
-	if err != nil {
-		return Chart{}, microerror.Mask(err)
-	}
-
-	return chart, nil
+	c.logger.LogCtx(ctx, "level", "debug", "message", "load chart not yet implemented for helm 3")
+	return Chart{}, nil
 }
 
 // RunReleaseTest runs the tests for a Helm Release. The releaseName is the
 // name of the Helm Release that is set when the Helm Chart is installed. This
 // is the same action as running the helm test command.
-func (c *Client) RunReleaseTest(ctx context.Context, releaseName string, options ...helmclient.ReleaseTestOption) error {
+func (c *Client) RunReleaseTest(ctx context.Context, releaseName string, options ReleaseTestOptions) error {
 	eventName := "run_release_test"
 
 	t := prometheus.NewTimer(histogram.WithLabelValues(eventName))
 	defer t.ObserveDuration()
 
-	err := c.runReleaseTest(ctx, releaseName, options...)
+	err := c.runReleaseTest(ctx, releaseName, options)
 	if err != nil {
 		errorGauge.WithLabelValues(eventName).Inc()
 		return microerror.Mask(err)
@@ -232,20 +220,20 @@ func (c *Client) RunReleaseTest(ctx context.Context, releaseName string, options
 	return nil
 }
 
-func (c *Client) runReleaseTest(ctx context.Context, releaseName string, options ...helmclient.ReleaseTestOption) error {
+func (c *Client) runReleaseTest(ctx context.Context, releaseName string, options ReleaseTestOptions) error {
 	c.logger.LogCtx(ctx, "level", "debug", "message", "run release test not yet implemented for helm 3")
 	return nil
 }
 
 // UpdateReleaseFromTarball updates the given release using the chart packaged
 // in the tarball.
-func (c *Client) UpdateReleaseFromTarball(ctx context.Context, releaseName, path string, options ...helmclient.UpdateOption) error {
+func (c *Client) UpdateReleaseFromTarball(ctx context.Context, releaseName, path string, options UpdateOptions) error {
 	eventName := "update_release_from_tarball"
 
 	t := prometheus.NewTimer(histogram.WithLabelValues(eventName))
 	defer t.ObserveDuration()
 
-	err := c.updateReleaseFromTarball(ctx, releaseName, path, options...)
+	err := c.updateReleaseFromTarball(ctx, releaseName, path, options)
 	if err != nil {
 		errorGauge.WithLabelValues(eventName).Inc()
 		return microerror.Mask(err)
@@ -254,19 +242,7 @@ func (c *Client) UpdateReleaseFromTarball(ctx context.Context, releaseName, path
 	return nil
 }
 
-func (c *Client) updateReleaseFromTarball(ctx context.Context, releaseName, path string, options ...helmclient.UpdateOption) error {
+func (c *Client) updateReleaseFromTarball(ctx context.Context, releaseName, path string, options UpdateOptions) error {
 	c.logger.LogCtx(ctx, "level", "debug", "message", "update release from tarball not yet implemented for helm 3")
 	return nil
-}
-
-func newChart(helmChart *hapichart.Chart) (Chart, error) {
-	if helmChart == nil || helmChart.Metadata == nil {
-		return Chart{}, microerror.Maskf(executionFailedError, "expected non nil argument but got %#v", helmChart)
-	}
-
-	chart := Chart{
-		Version: helmChart.Metadata.Version,
-	}
-
-	return chart, nil
 }
