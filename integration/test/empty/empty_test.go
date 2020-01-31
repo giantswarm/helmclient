a0a7c01f829c27ca6ae42618fe38e5f7a2b72d9a
Ross Fairbanks <ross@rossfairbanks.com>
2020-01-31 21:30:23 +0100

    Run e2e setup

---
+++ integration/test/empty/empty_test.go
@@ -0,0 +1,12 @@
+// +build k8srequired
+
+package empty
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestEmpty(t *testing.T) {
+	fmt.Println("Empty test for debugging orb")
+}
--- integration/test/empty/main_test.go
+++ integration/test/empty/main_test.go
@@ -5,10 +5,27 @@ package empty
 import (
 	"fmt"
 	"testing"
+
+	"github.com/giantswarm/helmclient/integration/setup"
 )
+
+var (
+	config setup.Config
+)
+
+func init() {
+	var err error
+
+	{
+		config, err = setup.NewConfig()
+		if err != nil {
+			panic(fmt.Sprintf("%#v", err))
+		}
+	}
+}
 
 // TestMain allows us to have common setup and teardown steps that are run
 // once for all the tests https://golang.org/pkg/testing/#hdr-Main.
 func TestMain(m *testing.M) {
-	fmt.Println("Empty test for debugging orb")
+	setup.Setup(m, config)
 }
