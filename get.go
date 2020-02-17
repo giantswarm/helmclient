79935c5af9e3f29391c493de4ae62ef1ba3b03b9
Ross Fairbanks <rossf7@users.noreply.github.com>
2020-02-17 17:41:48 +0100

    Fix param order for GetReleaseContent (#168)

--- get.go
+++ get.go
@@ -12,13 +12,13 @@ import (
 // GetReleaseContent gets the current status of the Helm Release including any
 // values provided when the chart was installed. The releaseName is the name
 // of the Helm Release that is set when the Helm Chart is installed.
-func (c *Client) GetReleaseContent(ctx context.Context, releaseName, namespace string) (*ReleaseContent, error) {
+func (c *Client) GetReleaseContent(ctx context.Context, namespace, releaseName string) (*ReleaseContent, error) {
 	eventName := "get_release_content"
 
 	t := prometheus.NewTimer(histogram.WithLabelValues(eventName))
 	defer t.ObserveDuration()
 
-	releaseContent, err := c.getReleaseContent(ctx, releaseName, namespace)
+	releaseContent, err := c.getReleaseContent(ctx, namespace, releaseName)
 	if err != nil {
 		errorGauge.WithLabelValues(eventName).Inc()
 		return nil, microerror.Mask(err)
@@ -27,7 +27,7 @@ func (c *Client) GetReleaseContent(ctx context.Context, releaseName, namespace s
 	return releaseContent, nil
 }
 
-func (c *Client) getReleaseContent(ctx context.Context, releaseName, namespace string) (*ReleaseContent, error) {
+func (c *Client) getReleaseContent(ctx context.Context, namespace, releaseName string) (*ReleaseContent, error) {
 	cfg, err := c.newActionConfig(ctx, namespace)
 	if err != nil {
 		return nil, microerror.Mask(err)
