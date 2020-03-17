75d9e4374a3694ec0d71667e4e218b43ecde70d8
Ross Fairbanks <rossf7@users.noreply.github.com>
2020-03-17 11:04:35 +0100

    helm3:  Add constants for statuses (#179)
    * Add constants for statuses
    
    * Add in transition statuses
    
    * Improve go doc
    
    * Add giant swarm specific not installed status
    
    * Remove not installed since its not a real status

--- spec.go
+++ spec.go
@@ -9,6 +9,43 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+// Describes the status of a release. This needs to be kept in sync with
+// upstream but it allows us to have constants without importing Helm
+// packages.
+//
+// See: https://github.com/helm/helm/blob/master/pkg/release/status.go
+const (
+	// StatusUnknown indicates that a release is in an uncertain state.
+	StatusUnknown = "unknown"
+	// StatusDeployed indicates that the release has been pushed to Kubernetes.
+	StatusDeployed = "deployed"
+	// StatusUninstalled indicates that a release has been uninstalled from Kubernetes.
+	StatusUninstalled = "uninstalled"
+	// StatusSuperseded indicates that this release object is outdated and a newer one exists.
+	StatusSuperseded = "superseded"
+	// StatusFailed indicates that the release was not successfully deployed.
+	StatusFailed = "failed"
+	// StatusUninstalling indicates that a uninstall operation is underway.
+	StatusUninstalling = "uninstalling"
+	// StatusPendingInstall indicates that an install operation is underway.
+	StatusPendingInstall = "pending-install"
+	// StatusPendingUpgrade indicates that an upgrade operation is underway.
+	StatusPendingUpgrade = "pending-upgrade"
+	// StatusPendingRollback indicates that an rollback operation is underway.
+	StatusPendingRollback = "pending-rollback"
+)
+
+var (
+	// ReleaseTransitionStatuses is used to determine if the Helm Release is
+	// currently being updated.
+	ReleaseTransitionStatuses = map[string]bool{
+		StatusUninstalled:     true,
+		StatusPendingInstall:  true,
+		StatusPendingUpgrade:  true,
+		StatusPendingRollback: true,
+	}
+)
+
 const (
 	// defaultMaxHistory is the maximum number of release versions stored per
 	// release by default.
@@ -19,7 +56,7 @@ const (
 	runReleaseTestTimout = 300
 )
 
-// Interface describes the methods provided by the helm client.
+// Interface describes the methods provided by the Helm client.
 type Interface interface {
 	// DeleteRelease uninstalls a chart given its release name.
 	DeleteRelease(ctx context.Context, namespace, releaseName string) error
@@ -48,6 +85,8 @@ type Interface interface {
 	UpdateReleaseFromTarball(ctx context.Context, chartPath, namespace, releaseName string, values map[string]interface{}, options UpdateOptions) error
 }
 
+// RESTClientGetter is used to configure the action package which is the Helm
+// Go client.
 type RESTClientGetter interface {
 	// ToDiscoveryClient returns discovery client
 	ToDiscoveryClient() (discovery.CachedDiscoveryInterface, error)
@@ -59,7 +98,7 @@ type RESTClientGetter interface {
 	ToRESTMapper() (meta.RESTMapper, error)
 }
 
-// InstallOptions is the subset of supported options when installing helm
+// InstallOptions is the subset of supported options when installing Helm
 // releases.
 type InstallOptions struct {
 	Namespace   string
@@ -67,7 +106,7 @@ type InstallOptions struct {
 	Wait        bool
 }
 
-// UpdateOptions is the subset of supported options when updating helm releases.
+// UpdateOptions is the subset of supported options when updating Helm releases.
 type UpdateOptions struct {
 	Force bool
 	Wait  bool
